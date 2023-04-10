a93377dcef1ee6b1c4165ae7145d1a22d2aa61d6
Timofii <unicondor.tv@gmail.com>
2023-04-10 18:27:50 +0300

    add App, Header, Main, Footer

---
+++ src/container/App/App.tsx
@@ -0,0 +1,17 @@
+import Footer from "container/Footer/Footer"
+import Header from "container/Header/Header"
+import Main from "container/Main/Main"
+
+type Props = {}
+
+const app = (props: Props) => {
+  return (
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+  )
+}
+
+export default app
---
+++ src/container/Main/Main.tsx
@@ -0,0 +1,9 @@
+type Props = {}
+
+const Main = (props: Props) => {
+  return (
+    <div>Main</div>
+  )
+}
+
+export default Main
